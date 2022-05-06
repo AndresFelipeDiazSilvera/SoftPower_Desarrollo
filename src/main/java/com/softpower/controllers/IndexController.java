dbc3563cf50ccec58c9fded4d16100b61558e0c4
juan david neira martinez <juan.neiram@usantoto.edu.co>
2022-05-06 10:59:57 -0500

    se agrego el navegar entre pestañas y se corrigio el boton despegable del menu

--- src/main/java/com/softpower/controllers/IndexController.java
+++ src/main/java/com/softpower/controllers/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
-    @GetMapping("/home")
+    @GetMapping("/inicio")
     public String index(){
         return "/inicio/inicio";
     }
